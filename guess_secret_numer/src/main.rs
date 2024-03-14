6349e135e84a1766f90106fc54473a15fd96dbba
Mikael <mikaelhadler@gmail.com>
2024-03-13 22:27:59 -0300

    put a break and user won

--- guess_secret_numer/src/main.rs
+++ guess_secret_numer/src/main.rs
@@ -3,23 +3,30 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    let mut guess = String::new();
-
-    println!("Guess the number!");
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read");
-
-    let guess:u32 = guess.trim().parse().expect("Please type a number");
-
-    println!("You guessed: {guess}");
-
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+    println!("Guess the number!");
+    loop {
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read");
+    
+        let guess:u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+    
+        println!("You guessed: {guess}");
+    
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
     }
 }
